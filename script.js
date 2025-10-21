440f8d3ac5a6bc7ff38589bd174c6297bd984454
collinapjoseph <collin.joseph.0258@gmail.com>
2025-10-21 18:00:13 -0400

    implemented phase 1 dice roller

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  $(".roll-btn").on("click", function () {
+    // get text input
+    var inputText = $(".dice-input").val();
+    console.log(`Input: ${inputText}`);
+
+    // Parse: dice quantity, dice value, modifier
+    // TODO: support unlimited sequence of dice and modifiers
+    var dice = inputText.match(
+      /(?<quantity>\d+)d(?<value>\d+)\s*(?<modSign>[\+\-])?\s*(?<modValue>\d+)?/
+    );
+
+    // compute each dice roll
+    var rollResult = 0;
+    var diceQuantity = Number(dice.groups.quantity);
+    var diceValue = Number(dice.groups.value);
+    for (var i = 0; i < diceQuantity; i++) {
+      var rollValue = Math.floor(Math.random() * diceValue) + 1;
+      rollResult += rollValue;
+      console.log(`Die #${i + 1}: ${rollValue}`);
+    }
+
+    // apply modifier
+    if (dice.groups.modSign) {
+      if (dice.groups.modSign == "+") {
+        rollResult += Number(dice.groups.modValue);
+      } else {
+        rollResult -= Number(dice.groups.modValue);
+      }
+      console.log(`Modifier: ${dice.groups.modSign}${dice.groups.modValue}`);
+    } else {
+      console.log("No modifier included.");
+    }
+
+    // set result text
+    $(".roll-result").text(rollResult);
+    $(".roll-result").css("visibility", "visible");
+  });
+});
